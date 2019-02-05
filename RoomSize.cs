0b224b515e0c7be1eb675fa9b2b9936024b94fcf
jkebx22 <42923140+jkebx22@users.noreply.github.com>
2019-02-05 17:41:50 -0600

    Add files via upload

---
+++ RoomSize.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelGo75
+{
+    class RoomSize
+    {
+    }
+}
